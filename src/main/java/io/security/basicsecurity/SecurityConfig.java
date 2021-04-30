5e0bfdf41c1e31ef8e5aab43d12abbc34c5a32df
seokjune <rhtjrwnszyzy@gmail.com>
2021-05-01 00:13:16 +0900

    Initial commit

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.security.basicsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();  //이게 인가 정책
+
+        http
+                .formLogin();   //이게 인증정책 기본 폼 로그인 방식으로 설정
+
+        http
+                .sessionManagement()
+                .sessionFixation().changeSessionId();
+    }
+}
